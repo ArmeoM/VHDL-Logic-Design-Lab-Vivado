a0f6c90c2f1e136c20eef6902034bf6144aeb260
ArmeoM <145188736+ArmeoM@users.noreply.github.com>
2026-03-11 23:41:52 +0300

    Update nbit_adder.vhd

--- day_1_ex/nbit_adder.vhd
+++ day_1_ex/nbit_adder.vhd
@@ -1,43 +1,49 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11.03.2026 16:40:41
--- Design Name: 
--- Module Name: nbit_adder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity nbit_adder is
---  Port ( );
+generic (
+N	: integer := 4
+); 
+port (
+a_i		: in std_logic_vector (N-1 downto 0);
+b_i		: in std_logic_vector (N-1 downto 0);
+carry_i	: in std_logic;
+sum_o	: out std_logic_vector (N-1 downto 0);
+carry_o	: out std_logic
+);
 end nbit_adder;
 
 architecture Behavioral of nbit_adder is
 
+-- COMPONENT DECLERATION
+component full_adder is
+port ( 
+a_i 	: in std_logic;
+b_i 	: in std_logic;
+carry_i : in std_logic;
+sum_o 	: out std_logic;
+carry_o : out std_logic
+);
+end component;
+
+signal temp	: std_logic_vector (N downto 0)	:= (others => '0');
+
 begin
+
+temp(0)	<= carry_i;
+carry_o	<= temp(N);
+
+FULL_ADDER_GEN: for k in 0 to N-1 generate
+	full_adder_k : full_adder
+	port map( 
+	a_i 	=> a_i(k),
+	b_i 	=> b_i(k),
+	carry_i => temp(k),
+	sum_o 	=> sum_o(k),
+	carry_o => temp(k+1)
+	);
+end generate;
 
 
 end Behavioral;
